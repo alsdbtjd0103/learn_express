25e13609a4608d46643f5d173bcee410a11cfd6f
심심풀이땅콩 <alsdbtjd89@gmail.com>
2022-04-04 07:41:34 +0000

    module화 완료

--- index.js
+++ index.js
@@ -25,64 +25,10 @@ db.on('error',(err) => {
 });
 
 //db schema
-var contactSchema = mongoose.Schema({
-	name:{type:String, required: true, unique:true},
-	email:{type:String},
-	phone:{type:String}
-})
-var Contact = mongoose.model('contact', contactSchema);
 
 //routes 설정
-app.get('/', (req,res) => {
-	res.redirect('/contacts');
-})
-//Contacts - Index 
-app.get('/contacts', (req,res) => {
-	Contact.find({},function(err,contacts){
-		if(err) return res.json(err);
-		res.render('contacts/index',{contacts:contacts});
-	});
-});
-//Contacts - New
-app.get('/contacts/new',(req,res) => {
-	res.render('contacts/new');
-});
-// Contacts - create 
-app.post('/contacts',(req,res) => {
-	Contact.create(req.body,(err,contact) => {
-		if(err) return res.json(err);
-		res.redirect('/contacts');
-	});
-});
-// Contacts - show
-app.get('/contacts/:id',(req,res) => {
-	Contact.findOne({_id:req.params.id},(err,contact) => {
-		if(err) return res.json(err);
-		res.render('contacts/show',{contact:contact});
-	});
-});
-// Contacts - edit
-app.get('/contacts/:id/edit',(req,res) => {
-	Contact.findOne({_id:req.params.id},(err,contact) => {
-		if(err) return res.json(err);
-		res.render('contacts/edit',{contact:contact});
-	})
-})
-// Contacts - update
-app.put('/contacts/:id',(req,res) => {
-	Contact.findOneAndUpdate({_id:req.params.id},req.body,(err,contact) => {
-		if(err) return res.json(err);
-		res.redirect('/contacts/'+req.params.id);
-	});
-});
-
-app.delete('/contacts/:id',(req,res) => {
-	Contact.deleteOne({_id:req.params.id},(err) => {
-		if(err) return res.json(err);
-		res.redirect('/contacts');
-	});
-});
-
+app.use('/',require('./routes/home'));
+app.use('/contacts',require('./routes/contacts'));
 
 
 
