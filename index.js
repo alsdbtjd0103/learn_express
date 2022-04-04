2b3279b5650b86088ebcc7f4b09887f183c32375
심심풀이땅콩 <alsdbtjd89@gmail.com>
2022-04-04 07:13:03 +0000

    restful success

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express= require('express');
 const app=express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const uri='mongodb+srv://ysyss:ysyss@cluster0.r3buu.mongodb.net/test?retryWrites=true&w=majority';
 
@@ -10,7 +11,7 @@ app.set('view engine','ejs');
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.json()); //json 형식의 데이터를 받음 -> 이걸 해야 req.body를 통해 form 데이터를 받을 수 있음]
 app.use(bodyParser.urlencoded({extended:true})); // extended 알고리즘 이용
-
+app.use(methodOverride('_method'));
 
 //db setting
 var db = mongoose.connection;
@@ -35,21 +36,53 @@ var Contact = mongoose.model('contact', contactSchema);
 app.get('/', (req,res) => {
 	res.redirect('/contacts');
 })
+//Contacts - Index 
 app.get('/contacts', (req,res) => {
 	Contact.find({},function(err,contacts){
 		if(err) return res.json(err);
 		res.render('contacts/index',{contacts:contacts});
 	});
 });
+//Contacts - New
 app.get('/contacts/new',(req,res) => {
 	res.render('contacts/new');
 });
+// Contacts - create 
 app.post('/contacts',(req,res) => {
 	Contact.create(req.body,(err,contact) => {
 		if(err) return res.json(err);
 		res.redirect('/contacts');
 	});
 });
+// Contacts - show
+app.get('/contacts/:id',(req,res) => {
+	Contact.findOne({_id:req.params.id},(err,contact) => {
+		if(err) return res.json(err);
+		res.render('contacts/show',{contact:contact});
+	});
+});
+// Contacts - edit
+app.get('/contacts/:id/edit',(req,res) => {
+	Contact.findOne({_id:req.params.id},(err,contact) => {
+		if(err) return res.json(err);
+		res.render('contacts/edit',{contact:contact});
+	})
+})
+// Contacts - update
+app.put('/contacts/:id',(req,res) => {
+	Contact.findOneAndUpdate({_id:req.params.id},req.body,(err,contact) => {
+		if(err) return res.json(err);
+		res.redirect('/contacts/'+req.params.id);
+	});
+});
+
+app.delete('/contacts/:id',(req,res) => {
+	Contact.deleteOne({_id:req.params.id},(err) => {
+		if(err) return res.json(err);
+		res.redirect('/contacts');
+	});
+});
+
 
 
 
